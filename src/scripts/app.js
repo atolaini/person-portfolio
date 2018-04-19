f8a6a015e523a1248fb6bade86801fb83e9c7f1a
Adam Tolaini <adam.tolaini@gmail.com>
2018-04-19 01:19:40 +0100

    java script functions working

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,18 @@
-$(function(){
+let x = document.getElementsByClassName('mySlides');
+let slideIndex = 1;
+showDivs(slideIndex);
 
-});
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  let i;
+  let x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
