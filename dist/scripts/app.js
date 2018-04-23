3b7f11b0bad63ac9de585602ea4bb7c10feee59e
Adam Tolaini <adam.tolaini@gmail.com>
2018-04-23 18:15:58 +0100

    finished

---
+++ dist/scripts/app.js
@@ -0,0 +1,142 @@
+let x = document.getElementsByClassName('mySlides');
+let slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  let i;
+  let x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
+
+
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
+  const navigation = document.querySelector('.mobileNavigation')
+  const navCheckbox = navigation.querySelector('.mobileNavigation__checkbox')
+  const navItems = navigation.querySelectorAll('.mobileNavigation__list--item')
+  navItems.forEach(item =>
+      item.addEventListener('click', _ => navCheckbox.checked = false))
+  
+
+
+  //Scroll Reveal effects
+
+  window.sr = ScrollReveal();
+  sr.reveal('.navBar__nav',{
+    origin: 'bottom',
+    duration: 1000,
+    distance: '100px',
+    opacity: 0,
+    easing: 'ease',
+    
+  });
+
+  sr.reveal('.homeSection__content--heading',{
+    origin: 'right',
+    duration: 1000,
+    distance: '500px',
+    opacity: 0,
+    easing: 'cubic-bezier(0.175, 0.885, 0.32, 1.275)',
+    reset: 'true'
+  });
+
+  sr.reveal('.homeSection__content--subHeading',{
+    duration: 1000,
+    delay: 900,
+    //distance: '500px',
+    opacity: 0,
+    easing: 'ease-out',
+    reset: 'true'
+  });
+
+  sr.reveal('.aboutSection__text', {
+    viewFactor: 0.85,
+    duration: 1200
+  })
+
+  // sr.reveal('.skillsSection', {
+  //   origin: 'bottom',
+  //   distance: '300px',
+  //   viewFactor: 0.2,
+  //   duration: 1200,
+  // })
+
+  sr.reveal('.gridItem--3', {
+    duration: 2000,
+    delay: 300,
+    easing: 'ease',
+    //reset: 'true',
+    viewFactor: .2,
+    opacity: 0
+  })
+
+  sr.reveal('.gridItem--2', {
+    duration: 1000,
+    easing: 'ease',
+    //reset: 'true',
+    viewFactor: .2,
+    opacity: 0
+  })
+
+  sr.reveal('.gridItem--1', {
+    duration: 1000,
+    delay: 700,
+    easing: 'ease',
+    //reset: 'true',
+    viewFactor: .2,
+    opacity: 0
+  })
+
+  
+  
+ 
+
+
+ 
+  
+
+
+  
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 let gulp = require ('gulp');
 let sass = require ('gulp-sass');
 let prefix = require ('gulp-autoprefixer');
+let htmlmin = require('gulp-htmlmin');
+let imagemin = require('gulp-imagemin');
+let cleanCSS = require('gulp-clean-css');
 
 gulp.task('sass', function(){
     return gulp.src(['src/scss/**/*.scss'], ['sass'])
@@ -8,6 +11,24 @@ gulp.task('sass', function(){
         .pipe(prefix('last 2 versions', 'Firefox > 20'))
         .pipe(gulp.dest('dist/css'))
 });
+
+gulp.task('minify', function() {
+    return gulp.src('src/index.html')
+      .pipe(htmlmin({collapseWhitespace: true}))
+      .pipe(gulp.dest('dist'));
+  });
+
+gulp.task('imgopt', () =>
+    gulp.src('src/img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/img'))
+);
+
+gulp.task('minify-css', () => {
+    return gulp.src('dist/css/*.css')
+      .pipe(cleanCSS({compatibility: 'ie8'}))
+      .pipe(gulp.dest('dist'));
+  });
 
 gulp.task('serve', ['sass'], function(){
     gulp.watch(['src/scss/**/*.scss'], ['sass'])
